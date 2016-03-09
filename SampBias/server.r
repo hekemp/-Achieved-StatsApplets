3f63a70d42bffe9930b591dbee18631033bfff4d
hekemp <heather-kemp@uiowa.edu>
2016-03-09 09:44:52 -0600

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -8,48 +8,34 @@ library(ggplot2)
 shinyServer(function(input, output) {
   
   # Computing proportion of orange candies
-  dataset <- reactive({
+  rawdataset <- reactive({
     if(input$goButton > 0 | TRUE) {
     data.frame(prob = rbinom(n = input$numsamp, size = input$sampsize, 
                     prob = input$popvalue)/input$sampsize)
     }
   })
+  print(rawdataset)
+  dataset <- rawdataset[order(mpg),] 
+  print(dataset)
   
-  output$plot <- renderPlot({
-    input$goButton
-    isolate({
-    rangeC <- max(dataset()$prob) - min(dataset()$prob)
+  # output$plot <- renderPlot({
+  #   input$goButton
+  #   isolate({
+  #   rangeC <- max(dataset()$prob) - min(dataset()$prob)
     
-    if(input$normcurve) {
+
+  #     # Building histogram of sampling distribution
+  #     p <- ggplot(dataset(), aes(x = prob)) 
+  #     p <- p + geom_histogram(fill = "steelblue", color = "black", 
+  #                             binwidth = rangeC/20) +
+  #       theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
+  #                 "; SE = ", 
+  #                 round(sqrt(mean(dataset()$prob)*
+  #                             (1-mean(dataset()$prob))/input$sampsize), 3)))
       
-      dataNorm <- data.frame(norm = rnorm(10000, mean = input$popvalue,
-                  sd = sqrt(((input$popvalue)*1-input$popvalue)/input$numsamp)))
-      # Building histogram of sampling distribution with normal curve
-      p <- ggplot(dataset(), aes(x = prob)) 
-      p <- p + geom_histogram(aes(y = ..density..), fill = "steelblue", 
-                              color = "black", binwidth = rangeC/20) +
-        geom_density(data = dataNorm, aes(x = norm), color = "darkgreen", 
-                     size = 1.25, alpha = 0) +
-        theme_bw(base_size = 24)+ labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
-                 "; SE = ", 
-                 round(sqrt(mean(dataset()$prob)*
-                              (1-mean(dataset()$prob))/input$sampsize), 3)))
-      
-      print(p)
-    } else {
-      # Building histogram of sampling distribution
-      p <- ggplot(dataset(), aes(x = prob)) 
-      p <- p + geom_histogram(fill = "steelblue", color = "black", 
-                              binwidth = rangeC/20) +
-        theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
-                  "; SE = ", 
-                  round(sqrt(mean(dataset()$prob)*
-                               (1-mean(dataset()$prob))/input$sampsize), 3)))
-      
-      print(p)
-    }
-    })   
-  })
-  
-})
+  #     print(p)
+    
+  #   })   
+  # })
+
                             
