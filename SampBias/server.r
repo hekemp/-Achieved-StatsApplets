c8ff0cac6e800aca72e8cb57f3363494a7de5bd7
hekemp <heather-kemp@uiowa.edu>
2016-03-09 13:52:58 -0600

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -22,11 +22,13 @@ shinyServer(function(input, output) {
     rangeC <- max(dataset()$prob) - min(dataset()$prob)
     
       # Building histogram of sampling distribution
-      p <- ggplot(dataset(), aes(x = dataset(), y = dataset$length)) 
-      p <- p + geom_point() + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
+      p <- ggplot(dataset(), aes(x = prob)) 
+      p <- p + geom_histogram(fill = "steelblue", color = "black", 
+                              binwidth = rangeC/20) +
+        theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
                   "; SE = ", 
                   round(sqrt(mean(dataset()$prob)*
-                              (1-mean(dataset()$prob))/input$sampsize), 3)))
+                               (1-mean(dataset()$prob))/input$sampsize), 3)))
       
       print(p)
     
