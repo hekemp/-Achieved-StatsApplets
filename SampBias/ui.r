690e54f90f647d2c5e1b4ecc539bacb9f3068d87
hekemp <heather-kemp@uiowa.edu>
2016-03-09 12:57:10 -0600

    Update ui.r

--- SampBias/ui.r
+++ SampBias/ui.r
@@ -12,7 +12,7 @@ shinyUI(pageWithSidebar(
 
     numericInput("sampsize", "Sample Size:", 25),
     numericInput("numsamp", "Number of Samples:", 100),
-    numericInput("popvalue", "Estimated Mean:", .45),
+    numericInput("popvalue", "Estimated Mean Between 0 and 1:", .45),
     sliderInput("biasval", "Estimated Bias:", -1.0, 1.0, 0.0, step = .01, round = FALSE, format = NULL, locale = NULL, ticks = TRUE, animate = FALSE, width = NULL, sep = ",", pre = NULL, post = NULL, timeFormat = NULL, timezone = NULL, dragRange = TRUE),
     
     actionButton("goButton", "Go!")),
