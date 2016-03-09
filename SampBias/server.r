89fe69ead77abfa34d283b1f5e3a3ef266be08ba
hekemp <heather-kemp@uiowa.edu>
2016-03-09 09:51:19 -0600

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -8,34 +8,31 @@ library(ggplot2)
 shinyServer(function(input, output) {
   
   # Computing proportion of orange candies
-  rawdataset <- reactive({
+  dataset <- reactive({
     if(input$goButton > 0 | TRUE) {
     data.frame(prob = rbinom(n = input$numsamp, size = input$sampsize, 
                     prob = input$popvalue)/input$sampsize)
     }
   })
-  dataset <- rawdataset[order(mpg),] 
   
   output$plot <- renderPlot({
     input$goButton
     isolate({
     rangeC <- max(dataset()$prob) - min(dataset()$prob)
-    
 
       # Building histogram of sampling distribution
-      p <- plot(dataset(), aes(x = prob)) 
-      p <- p + geom_histogram(aes(y = ..density..), fill = "steelblue", 
-                              color = "black", binwidth = rangeC/20) +
-        geom_density(data = dataNorm, aes(x = norm), color = "darkgreen", 
-                     size = 1.25, alpha = 0) +
-        theme_bw(base_size = 24)+ labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
+      p <- ggplot(dataset(), aes(x = prob)) 
+      p <- p + geom_histogram(fill = "steelblue", color = "black", 
+                              binwidth = rangeC/20) +
+        theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
                   "; SE = ", 
                   round(sqrt(mean(dataset()$prob)*
-                              (1-mean(dataset()$prob))/input$sampsize), 3)))
+                               (1-mean(dataset()$prob))/input$sampsize), 3)))
       
       print(p)
     
     })   
   })
-
+  
+})
                             
