cbfb02a7e9a08b9a6771481554372a159f00db03
hekemp <heather-kemp@uiowa.edu>
2016-03-10 14:13:54 -0600

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -15,35 +15,36 @@ shinyServer(function(input, output) {
   #   }
   # })
     # Computing proportion of orange candies
-  dataset <- reactive({
-    if(input$goButton > 0 | TRUE) {
-    replicate(input$numsamp, 
-              rnorm(n = input$sampsize, mean = input$popmean, sd = input$popsd))
-    }
-  })
-  output$dataset <- renderDataTable({dataset)}
+  # dataset <- reactive({
+  #   if(input$goButton > 0 | TRUE) {
+  #   replicate(input$numsamp, 
+  #             rnorm(n = input$sampsize, mean = input$popmean, sd = input$popsd))
+  #   }
+  # })
+  # output$dataset <- renderDataTable({dataset)}
+  dataset <- msleep
 
   
-    output$plot <- renderPlot({
-    input$goButton
-    # compute means and sd for data
-    isolate({
-    confnums <- data.frame(means = apply(dataset(), 2, mean),
-                           se = apply(dataset(), 2, sd)/sqrt(input$sampsize))
+    # output$plot <- renderPlot({
+    # input$goButton
+    # # compute means and sd for data
+    # isolate({
+    # confnums <- data.frame(means = apply(dataset(), 2, mean),
+    #                       se = apply(dataset(), 2, sd)/sqrt(input$sampsize))
   
-  # output$plot <- renderPlot({
-  #   input$goButton
-  #   isolate({
-  #   rangeC <- max(dataset()$prob) - min(dataset()$prob)
+  output$plot <- renderPlot({
+    input$goButton
+    isolate({
+#    rangeC <- max(dataset()$prob) - min(dataset()$prob)
     
-  #     # Building histogram of sampling distribution
-  #     p <- ggplot(dataset(), aes(x = prob)) 
-  #     p <- p + geom_histogram(fill = "steelblue", color = "black", 
-  #                             binwidth = rangeC/20) +
-  #       theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
-  #                 "; SE = ", 
-  #                 round(sqrt(mean(dataset()$prob)*
-  #                             (1-mean(dataset()$prob))/input$sampsize), 3)))
+      # Building histogram of sampling distribution
+      p <- ggplot(dataset(), aes(x = dataset[5], y = dataset[10])) 
+      # p <- p + geom_histogram(fill = "steelblue", color = "black", 
+      #                         binwidth = rangeC/20) +
+      #   theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
+      #             "; SE = ", 
+      #             round(sqrt(mean(dataset()$prob)*
+      #                         (1-mean(dataset()$prob))/input$sampsize), 3)))
       
       print(p)
     
