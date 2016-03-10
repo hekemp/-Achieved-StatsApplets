916043000298186217b3f74f8ef7cf1e417cb1b0
hekemp <heather-kemp@uiowa.edu>
2016-03-10 14:01:18 -0600

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -7,27 +7,41 @@ library(ggplot2)
 
 shinyServer(function(input, output) {
   
-  # Computing proportion of orange candies
+  # # Computing proportion of orange candies
+  # dataset <- reactive({
+  #   if(input$goButton > 0 | TRUE) {
+  #   data.frame(prob = rbinom(n = input$numsamp, size = input$sampsize, 
+  #                   prob = input$popvalue)/input$sampsize)
+  #   }
+  # })
+    # Computing proportion of orange candies
   dataset <- reactive({
     if(input$goButton > 0 | TRUE) {
-    data.frame(prob = rbinom(n = input$numsamp, size = input$sampsize, 
-                    prob = input$popvalue)/input$sampsize)
+    replicate(input$numsamp, 
+              rnorm(n = input$sampsize, mean = input$popmean, sd = input$popsd))
     }
   })
   
-  output$plot <- renderPlot({
+    output$plot <- renderPlot({
     input$goButton
+    # compute means and sd for data
     isolate({
-    rangeC <- max(dataset()$prob) - min(dataset()$prob)
+    confnums <- data.frame(means = apply(dataset(), 2, mean),
+                           se = apply(dataset(), 2, sd)/sqrt(input$sampsize))
+  
+  # output$plot <- renderPlot({
+  #   input$goButton
+  #   isolate({
+  #   rangeC <- max(dataset()$prob) - min(dataset()$prob)
     
-      # Building histogram of sampling distribution
-      p <- ggplot(dataset(), aes(x = prob)) 
-      p <- p + geom_histogram(fill = "steelblue", color = "black", 
-                              binwidth = rangeC/20) +
-        theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
-                  "; SE = ", 
-                  round(sqrt(mean(dataset()$prob)*
-                               (1-mean(dataset()$prob))/input$sampsize), 3)))
+  #     # Building histogram of sampling distribution
+  #     p <- ggplot(dataset(), aes(x = prob)) 
+  #     p <- p + geom_histogram(fill = "steelblue", color = "black", 
+  #                             binwidth = rangeC/20) +
+  #       theme_bw(base_size = 24) + labs(title = paste("Mean = ", round(mean(dataset()$prob), 3), 
+  #                 "; SE = ", 
+  #                 round(sqrt(mean(dataset()$prob)*
+  #                             (1-mean(dataset()$prob))/input$sampsize), 3)))
       
       print(p)
     
