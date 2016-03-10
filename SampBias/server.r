5bde5b715f5ae4d7adf703d278cc0ebf84832e9a
hekemp <heather-kemp@uiowa.edu>
2016-03-10 14:02:56 -0600

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -21,7 +21,7 @@ shinyServer(function(input, output) {
               rnorm(n = input$sampsize, mean = input$popmean, sd = input$popsd))
     }
   })
-  print(dataset)
+
   
     output$plot <- renderPlot({
     input$goButton
@@ -45,6 +45,7 @@ shinyServer(function(input, output) {
   #                             (1-mean(dataset()$prob))/input$sampsize), 3)))
       
       print(p)
+      print(dataset)
     
     })   
   })
