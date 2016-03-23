76a2c32f3a3c9849c65981a85a6630c5b4b728a5
hekemp <heather-kemp@uiowa.edu>
2016-03-23 09:47:48 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -5,46 +5,27 @@
 library(shiny)
 library(ggplot2)
 
-meanvals <- mtcars$mpg
-meannum <- mean(meanvals)
-
 shinyServer(function(input, output) {# For storing which rows have been excluded
   vals <- reactiveValues(
     keeprows = rep(TRUE, nrow(mtcars))
   )
-  
-  getGraph <- function(){renderPlot({
+
+  output$plot1 <- renderPlot({
     # Plot the kept and excluded points as two separate data sets
     keep    <- mtcars[ vals$keeprows, , drop = FALSE]
     exclude <- mtcars[!vals$keeprows, , drop = FALSE]
     
-    p <- ggplot(keep, aes(wt, mpg)) + geom_point() +
+    ggplot(keep, aes(wt, mpg)) + geom_point() +
        geom_point(data = exclude, fill = NA, color = "black", alpha = 0.25) +
       coord_cartesian(xlim = c(1.5, 5.5), ylim = c(5,35))
-      p <- p + ggtitle(paste("Mean = ", meannum, " SD = "))
-      print(p)
       #+ labs(title = "Mean = "#paste("Mean = ", round(mean(mtcars), 3), "; SE = ", round(sqrt(mean(mtcars)* (1-mean(mtcars))/input$sampsize), 3)))
-  })}
-
-  output$plot1 <- getGraph()#renderPlot({
-    # Plot the kept and excluded points as two separate data sets
-#    keep    <- mtcars[ vals$keeprows, , drop = FALSE]
-#    exclude <- mtcars[!vals$keeprows, , drop = FALSE]
-#    
-#    p <- ggplot(keep, aes(wt, mpg)) + geom_point() +
-#       geom_point(data = exclude, fill = NA, color = "black", alpha = 0.25) +
-#      coord_cartesian(xlim = c(1.5, 5.5), ylim = c(5,35))
-#      p <- p + ggtitle(paste("Mean = ", meannum, " SD = "))
-#      print(p)
-#      #+ labs(title = "Mean = "#paste("Mean = ", round(mean(mtcars), 3), "; SE = ", round(sqrt(mean(mtcars)* (1-mean(mtcars))/input$sampsize), 3)))
-#  })
+  })
 
   # Toggle points that are clicked
   observeEvent(input$plot1_click, {
     res <- nearPoints(mtcars, input$plot1_click, allRows = TRUE)
 
     vals$keeprows <- xor(vals$keeprows, res$selected_)
-    getGraph()
   })
 
   # Toggle points that are brushed, when button is clicked
@@ -52,13 +33,11 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
     res <- brushedPoints(mtcars, input$plot1_brush, allRows = TRUE)
 
     vals$keeprows <- xor(vals$keeprows, res$selected_)
-    getGraph()
   })
 
   # Reset all points
   observeEvent(input$exclude_reset, {
     vals$keeprows <- rep(TRUE, nrow(mtcars))
-    getGraph()
   })
 })
 
