0751e6e967c519329b3e72049c1a8471dc78cd26
hekemp <heather-kemp@uiowa.edu>
2016-03-23 17:12:27 -0500

    Update ui.r

--- SampBias/ui.r
+++ SampBias/ui.r
@@ -10,14 +10,10 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
   
-    helpText("Currently the graph is running on pre-set data, so all of these fields aren't active at the moment. To see the effects of bias just click the point that will be removed."),
+    helpText("To see the effects of bias just click the point that will be removed."),
     helpText("To use toggle points, highlight the area of points in question and then hit toggle points. This will invert the selected points' statuses."),
-    numericInput("popmean", "Population Mean:", 0),
-    numericInput("popsd", "Population SD:", 1),
     numericInput("sampsize", "Sample Size:", 25),
-    numericInput("numsamp", "Number of Samples:", 100),
-    sliderInput("biasval", "Percent of Estimated Bias:", -1.0, 1.0, 0.0, step = .01, round = FALSE, format = NULL, locale = NULL, ticks = TRUE, animate = FALSE, width = NULL, sep = ",", pre = NULL, post = NULL, timeFormat = NULL, timezone = NULL, dragRange = TRUE),
-    
+
     actionButton("goButton", "Go!")),
   
   mainPanel(
