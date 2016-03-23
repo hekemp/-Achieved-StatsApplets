5e7ef3a7c027d93f59a6835c02cd38d7f38a8f5e
hekemp <heather-kemp@uiowa.edu>
2016-03-23 12:52:07 -0500

    Update ui.r

--- SampBias/ui.r
+++ SampBias/ui.r
@@ -21,16 +21,27 @@ shinyUI(pageWithSidebar(
     actionButton("goButton", "Go!")),
   
   mainPanel(
-      textOutput("meansd1"),
-      plotOutput("plot1", height = 350,
-        click = "plot1_click",
-        brush = brushOpts(
-          id = "plot1_brush"
+    tabsetPanel(
+     tabPanel("Plot",
+       fluidRow(
+          column(8, plotOutput("plot1", height = 350,
+           click = "plot1_click",
+           brush = brushOpts(
+           id = "plot1_brush"
         )
-      ),
+      )),
+          column(12, plotOutput("plot2"))
+         ))
+#      textOutput("meansd1"),
+#      plotOutput("plot1", height = 350,
+#        click = "plot1_click",
+#        brush = brushOpts(
+#          id = "plot1_brush"
+#        )
+#      ),
       actionButton("exclude_toggle", "Toggle points"),
       actionButton("exclude_reset", "Reset")
     )
   )
-)
+))
     
