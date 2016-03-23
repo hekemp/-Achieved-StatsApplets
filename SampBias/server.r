9f503e66c58e3df68bc3127daf78b7ded688d441
hekemp <heather-kemp@uiowa.edu>
2016-03-23 08:04:59 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -17,7 +17,7 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
     
     ggplot(keep, aes(wt, mpg)) + geom_point() +
 #      geom_smooth(method = lm, fullrange = TRUE, color = "black") +
-      + geom_point(data = exclude, fill = NA, color = "black", alpha = 0.25) +
+       geom_point(data = exclude, fill = NA, color = "black", alpha = 0.25) +
       coord_cartesian(xlim = c(1.5, 5.5), ylim = c(5,35))
 #      + labs(title = paste("Mean = ", round(mean(mtcars), 3), "; SE = ", round(sqrt(mean(mtcars)* (1-mean(mtcars))/input$sampsize), 3)))
   })
