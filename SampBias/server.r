35986d73f24185ba6b06acbf63b345334fc9c64d
hekemp <heather-kemp@uiowa.edu>
2016-03-23 11:46:19 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -42,11 +42,10 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
   output$meansd1 <- renderText({
   mpgdata <- mtcars$mpg
   meanval <- mean(mpgdata)
-  sdval <- sqrt(mean(mpgdata)* (1-mean(mpgdata))/nrow(mtcars))
+  sdval <- sd(mpgdata)
 #  result <- "Mean = " + mean(mpgdata) + " SD = " + round(sqrt(mean(mpgdata)* (1-mean(mpgdata))/nrow(mtcars), 3))
  # print(result)
- print(meanval)
- print(sdval)
+ "Mean = " + meanval + " SD = " + sdval
   })
 
 })
