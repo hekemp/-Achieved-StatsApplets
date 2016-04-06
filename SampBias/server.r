7919cc24d42526f0b2fee1509f0da0110e9d7356
hekemp <heather-kemp@uiowa.edu>
2016-04-05 21:43:00 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -48,10 +48,10 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
     {vals$keeprows <- rep(TRUE, nrow(baboon))}
     if(input$selection == "armLength")
     {vals$keeprows <- rep(TRUE, nrow(baboon))
-     res <- subset(baboon, (upperarm < 15))
-     vals$keeprows <- res #vals$keeprows[res]
+     res <- which(baboon$upperarm < 15)
+#     vals$keeprows <- res #vals$keeprows[res]
 #     vals$keeprows <- subset(vals$keeprows, (upperarm < 15))
-#    vals$keeprows <- xor(vals$keeprows, res)
+    vals$keeprows <- xor(vals$keeprows, res)
     }
     
     if(input$selection == "age")
