cf0926531d5d74a02cafd2e4c990fe77e9df2db5
hekemp <heather-kemp@uiowa.edu>
2016-04-05 20:27:02 -0500

    Update ui.r

--- SampBias/ui.r
+++ SampBias/ui.r
@@ -16,7 +16,7 @@ shinyUI(pageWithSidebar(
     helpText("To use toggle points, highlight the area of points in question and then hit toggle points. This will invert the selected points' statuses."),
     numericInput("sampsize", "Sample Size:", 25),
     helpText("To see examples of pre-established biases, select one of the following variables."),
-    helpText("Note: Selecting a variable aside from 'Default' will reset the graph's settings."),
+    helpText("Note: Changing this variable will reset the graph's settings."),
     selectInput("selection", "Select what variable there will be bias for:", choices = biasChoices)),
   
   mainPanel(
