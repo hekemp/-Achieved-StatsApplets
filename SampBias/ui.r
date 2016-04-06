c6c5f70c0847fff4f8c9035adad5b3e60e7c9f71
hekemp <heather-kemp@uiowa.edu>
2016-04-05 20:23:49 -0500

    Update ui.r

--- SampBias/ui.r
+++ SampBias/ui.r
@@ -3,7 +3,7 @@
 # to a shiny app.
 
 library(shiny)
-
+biasChoices <<- list("Upper Arm Length" = "armLength", "Age" = "age", "Skinfold Depth" = "skinfold"),
 shinyUI(pageWithSidebar(
   
   headerPanel("Sampling Bias"),
@@ -13,7 +13,6 @@ shinyUI(pageWithSidebar(
     helpText("To see the effects of bias just click the point that will be removed."),
     helpText("To use toggle points, highlight the area of points in question and then hit toggle points. This will invert the selected points' statuses."),
     numericInput("sampsize", "Sample Size:", 25),
-    biasChoices <<- list("Upper Arm Length" = "armLength", "Age" = "age", "Skinfold Depth" = "skinfold"),
     selectInput("selection", "Select what variable there will be bias for:", choices = biasChoices)),
   
   mainPanel(
