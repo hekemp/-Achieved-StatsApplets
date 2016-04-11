e75b7376ebca127af373ad76bb95445f8851183d
hekemp <heather-kemp@uiowa.edu>
2016-04-11 10:04:25 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -92,7 +92,8 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
   
   output$plot3 <- renderPlot({
   randSamp <- getSample()
-  pl <- ggplot(randSamp, aes(length, mass)) + geom_point() +  coord_cartesian(xlim = c(66, 157), ylim = c(7,30)) + geom_smooth(method = lm, data = round(mean(data.table(randSamp$mass)),3), fullrange = TRUE, shape = 21, color = "black")
+  pl <- ggplot(randSamp, aes(length, mass)) + geom_point() +  coord_cartesian(xlim = c(66, 157), ylim = c(7,30)) + abline(x = mean(randSamp$mass))
+  print(pl)
   })
   
   })
