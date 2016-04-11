2b3515c2a6d6c9a4bd59c81b60fc3d89bc567b8c
hekemp <heather-kemp@uiowa.edu>
2016-04-11 09:56:40 -0500

    Update ui.r

--- SampBias/ui.r
+++ SampBias/ui.r
@@ -27,7 +27,8 @@ shinyUI(pageWithSidebar(
       plotOutput("plot1", height = 350, click = "plot1_click", brush = brushOpts(id = "plot1_brush")),
       actionButton("exclude_toggle", "Toggle points"),
       actionButton("exclude_reset", "Reset"),
-      plotOutput("plot2")
+      plotOutput("plot2"),
+      plotOutput("plot3")
     )
   )
 )
