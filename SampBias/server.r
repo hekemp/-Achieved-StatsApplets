8a968c4bb8ed100fa5f42abb444a96c012b5395c
hekemp <heather-kemp@uiowa.edu>
2016-04-13 17:06:17 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -141,7 +141,7 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
     }
   else if(length(val$meanDataSet) == 1)
   {#bins <- seq(min(val$meanDataSet), max(val$meanDataSet), length.out = input$numBins + 1)
-     hist(val$meanDataSet, breaks = 1, col = 'darkgray', border = 'white', main = "Histogram of Mean Masses From Samples", xlab = "Mean of Sample", ylab = "Frequency", xlim = c(0,30), ylim=c(0, 2))
+     hist(val$meanDataSet, breaks = input$numBins, col = 'darkgray', border = 'white', main = "Histogram of Mean Masses From Samples", xlab = "Mean of Sample", ylab = "Frequency", xlim = c(0,30), ylim=c(0, 2))
      abline(v=mean(baboon$mass),col="red")
      }
     
