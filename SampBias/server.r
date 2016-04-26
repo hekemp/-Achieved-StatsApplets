5956b65bd6e6fdd5c312b7c50c0cd397452f9de5
hekemp <heather-kemp@uiowa.edu>
2016-04-25 22:39:47 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -23,6 +23,7 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
       baboon <- read.csv("baboonsM.csv")
     if(input$popSelect == "females")
       baboon <- read.csv("baboonsF.csv")
+      })
   
   output$plot1 <- renderPlot({
     # Plot the kept and excluded points as two separate data sets
