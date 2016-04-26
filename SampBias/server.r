280f0cd1b4093a82adb291c1089a03a82f2abc12
hekemp <heather-kemp@uiowa.edu>
2016-04-25 23:05:57 -0500

    Update server.r

--- SampBias/server.r
+++ SampBias/server.r
@@ -27,7 +27,7 @@ shinyServer(function(input, output) {# For storing which rows have been excluded
       })
   
   vals <- reactiveValues(
-    keeprows = rep(TRUE, nrow(baboon))
+    keeprows = rep(TRUE, nrow(baboon()))
   )
   val <- reactiveValues(
     meanDataSet = c()
