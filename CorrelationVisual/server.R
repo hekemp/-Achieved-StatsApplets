b2a0bfe7e6cc1704090ca7ae82a9aa625ea3bff0
hekemp <heather-kemp@uiowa.edu>
2016-09-22 14:36:45 -0500

    Update server.R

--- CorrelationVisual/server.R
+++ CorrelationVisual/server.R
@@ -18,7 +18,7 @@ rho  <- input$rho
 shots <- mvrnorm(n=as.numeric(input$nr_obs),mu=c(mu1,mu2),Sigma=matrix(c(sig1,rho,rho,sig2),2))
 
 # Plot the shots
-plot(shots,xlim=c(-4,4),ylim=c(-4,4),xlab="y1",ylab="y2",col="dark blue",pch=20)
+plot(shots,xlim=c(-4,4),ylim=c(-4,4),xlab="x",ylab="y",col="dark blue",pch=20)
 
 
   })  
