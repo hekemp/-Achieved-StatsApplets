db42809201dd12f27bfe078cc659d090581669bd
hekemp <heather-kemp@uiowa.edu>
2016-09-22 14:31:34 -0500

    Update ui.R

--- CorrelationGame/ui.R
+++ CorrelationGame/ui.R
@@ -9,11 +9,11 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
 #    "INPUTS",
 
-    sliderInput("rho", "Corrlelation (rho):", min = 0, max = 1, value = 0.5, step=0.01),
+    sliderInput("rho", "Corrlelation:", min = 0, max = 1, value = 0.5, step=0.01),
     selectInput("nr_obs", "Number of observations:", c(50,100,500,1000,10000,100000), selected =1000, multiple = FALSE),
     br(),
     br(),
-    "Note: The observations are distributed accorting to a standard bivariate distribution with correlation rho."
+    "Note: The observations are distributed accorting to a standard bivariate distribution with correlation r."
     ),
 
   mainPanel(plotOutput("scatterplot",height="800px"))
