75278d2e3fed4daf5220c0e850d062cf835cdb6e
hekemp <heather-kemp@uiowa.edu>
2016-09-22 14:37:30 -0500

    Update ui.R

--- CorrelationVisual/ui.R
+++ CorrelationVisual/ui.R
@@ -12,7 +12,6 @@ shinyUI(pageWithSidebar(
     sliderInput("rho", "Corrlelation:", min = 0, max = 1, value = 0.5, step=0.01),
     selectInput("nr_obs", "Number of observations:", c(50,100,500,1000,10000,100000), selected =1000, multiple = FALSE),
     br(),
-    br(),
     "Note: The observations are distributed accorting to a standard bivariate distribution with correlation r."
     ),
 
