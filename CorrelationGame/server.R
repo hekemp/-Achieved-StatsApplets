024ed8392d4b8a09890426c3c4f45f29543a8646
hekemp <heather-kemp@uiowa.edu>
2016-09-28 22:29:10 -0500

    Update server.R

--- CorrelationGame/server.R
+++ CorrelationGame/server.R
@@ -20,21 +20,21 @@ val <- reactiveValues(
   
    # if(abs(input$rho) - abs(vals$rValue) < .3 || abs(input$rho) - abs(vals$rValue) > -.3 )
    if(input$rho - vals$rValue < .1 & input$rho - vals$rValue > -.1)
-   {val$messageToReturn <- "That's correct!"}
+   {val$messageToReturn <- "That's correct! R = " + + vals$rValue}
     if(input$rho - vals$rValue > .1 || input$rho - vals$rValue < -.1)
       {if(input$rho - vals$rValue <= 1 || input$rho - vals$rValue >= -1)
         {if(vals$rValue < input$rho)
-            {val$messageToReturn <- "That guess was too high. Try again!"}
+            {val$messageToReturn <- "That guess was too high. R = " + + vals$rValue}
 
          if(vals$rValue > input$rho)
-          {val$messageToReturn <- "That guess was too low. Try again!"}
+          {val$messageToReturn <- "That guess was too low. R = " + vals$rValue}
       
       if((input$rho - vals$rValue <= .3 & input$rho - vals$rValue >= .1) || (input$rho - vals$rValue >= -.3 & input$rho - vals$rValue <= -.1))
         {if(vals$rValue < input$rho)
-            {val$messageToReturn <- "That guess was a little too high. Try again!"}
+            {val$messageToReturn <- "That guess was a little too high. R = " + + vals$rValue}
 
          if(vals$rValue > input$rho)
-          {val$messageToReturn <- "That guess was a little too low. Try again!"}
+          {val$messageToReturn <- "That guess was a little too low. R = " + vals$rValue}
 
         }
       }
