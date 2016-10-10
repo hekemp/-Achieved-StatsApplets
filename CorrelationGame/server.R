c5d532aa380cc29bd98a8c6b7b5d400536e6d180
hekemp <heather-kemp@uiowa.edu>
2016-10-09 20:39:13 -0500

    Update server.R

--- CorrelationGame/server.R
+++ CorrelationGame/server.R
@@ -135,7 +135,7 @@ plot(shots*100,xlim=c(0,100),ylim=c(0,100),xlab="x",ylab="y",col="dark blue",pch
   })
   
     output$results <- renderPlot({
-  plot(seq(1,valu$answerChecked), valuPlot$numRight)
+  plot(x = seq(1,valu$answerChecked), y = valuPlot$numRight)
 })
 
 })
