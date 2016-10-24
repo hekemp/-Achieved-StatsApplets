7348ba56da3f286ec6af35e58157a8a6290890f4
hekemp <heather-kemp@uiowa.edu>
2016-10-24 15:06:56 -0500

    Update server.R

--- CorrelationGame/server.R
+++ CorrelationGame/server.R
@@ -145,7 +145,7 @@ rho  <- vals$rValue
 shots <- mvrnorm(n=as.numeric(nr_obs),mu=c(mu1,mu2),Sigma=matrix(c(sig1,rho,rho,sig2),2))
 
 # Plot the shots
-plot(abs(shots), xlim=c(0,4),ylim=c(0,4), xlab="x",ylab="y",col="dark blue",pch=20)
+plot(shots, xlim=c(-4,4),ylim=c(-4,4), xlab="x",ylab="y",col="dark blue",pch=20)
 
 
   })
