507962625d7be31f169d25ddd76708c2cc7dc763
hekemp <heather-kemp@uiowa.edu>
2016-10-24 16:07:39 -0500

    Update server.R

--- CorrelationGame/server.R
+++ CorrelationGame/server.R
@@ -175,11 +175,11 @@ plot(shots, xlim=c(-4,4),ylim=c(-4,4), xlab="x",ylab="y",col="dark blue",pch=20)
       print(length(guessPlot$guess))
       negThrees = {}
       negThrees[1] = -3
-      for(i in 2:length(guessPlot$guess)-1){
+      for(i in 2:length(guessPlot$guess)){
         negThrees[i] = guessPlot$guess[i]
         print(length(negThrees))
       }
-      for(i in length(guessPlot$guess):21){
+      for(i in length(guessPlot$guess)+1:21){
         negThrees[i] = -3
         print(length(negThrees))
       }
