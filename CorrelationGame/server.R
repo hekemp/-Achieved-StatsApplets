edeae150a6dcc9052310a47d92b06d1f2e852123
hekemp <heather-kemp@uiowa.edu>
2016-10-24 15:57:57 -0500

    Update server.R

--- CorrelationGame/server.R
+++ CorrelationGame/server.R
@@ -181,7 +181,7 @@ plot(shots, xlim=c(-4,4),ylim=c(-4,4), xlab="x",ylab="y",col="dark blue",pch=20)
       for(i in length(guessPlot$guess)+1:21){
         negThrees[i] = -3
       }
-            print(length(negThrees)
+            print(length(negThrees))
       
    plot(x = seq(0,20), y = negThrees, ylim=c(-2,2))
    abline(h=0)}
